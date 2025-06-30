2c6e721b6a027c612c346171a6fd2363001f6d2a
Murilo Zamaioli <murilo.zamaioli@gmail.com>
2025-06-30 20:18:31 -0300

    cabeçalho

--- index.php
+++ index.php
@@ -1,11 +1,52 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt-BR">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Imobiliária</title>
+    <link rel="stylesheet" href="estilo.css" />
 </head>
+
 <body>
-    
+    <div class="divpai">
+        <header class="cabecalho" >
+            <div class="cabecalho-conteudo">
+                <div class="menu-logo">
+                    <img src="imagens/logo.png.png" alt="Logo Morada Prime" class="logo" />
+                    <h1>Morada Prime</h1>
+                </div>
+                <nav class="menu-esquerda">
+                    <a href="#" class="menu-cabecalho">Início</a>
+                    <a href="#" class="menu-cabecalho">Serviços</a>
+                    <a href="#" class="menu-cabecalho">Sobre</a>
+                    <a href="#" class="menu-cabecalho">Contatos</a>
+                </nav>
+                <a href="login.html" class="menu-cabecalho entrar">Entrar em Contato</a>
+            </div>
+        </header>
+
+        <section class="banner">
+            <h2>Seu novo lar te espera</h2>
+            <p>Casas, apartamentos e terrenos à venda</p>
+        </section>
+
+        <section class="imoveis">
+            <h3>Imóveis em Destaque</h3>
+            <div class="card">
+                <img src="https://via.placeholder.com/300x200" alt="Casa 1">
+                <p><strong>Casa 3 Quartos</strong><br>R$ 350.000</p>
+            </div>
+            <div class="card">
+                <img src="https://via.placeholder.com/300x200" alt="Apartamento 2">
+                <p><strong>Apartamento 2 Quartos</strong><br>R$ 250.000</p>
+            </div>
+        </section>
+    </div>
+    <footer>
+        <p>&copy; 2025 Imobiliária Simples. Todos os direitos reservados.</p>
+    </footer>
+
 </body>
+
 </html>
