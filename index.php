8c0041bd1e1269a5dac17540eb61715819eebff9
Murilo Zamaioli <murilo.zamaioli@gmail.com>
2025-06-30 22:24:54 -0300

    conexao feita, só falta organizar

--- index.php
+++ index.php
@@ -1,52 +1,57 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Imobiliária</title>
-    <link rel="stylesheet" href="estilo.css" />
-</head>
-
-<body>
-    <div class="divpai">
-        <header class="cabecalho" >
-            <div class="cabecalho-conteudo">
-                <div class="menu-logo">
-                    <img src="imagens/logo.png.png" alt="Logo Morada Prime" class="logo" />
-                    <h1>Morada Prime</h1>
+<?php include 'cabecalho.php'
+?>
+    <div class="banner">
+        <div id="carouselExample" class="carousel slide">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="imagens/casa1.jpg" class="d-block w-100 imagem-carrossel" alt="casa1">
                 </div>
-                <nav class="menu-esquerda">
-                    <a href="#" class="menu-cabecalho">Início</a>
-                    <a href="#" class="menu-cabecalho">Serviços</a>
-                    <a href="#" class="menu-cabecalho">Sobre</a>
-                    <a href="#" class="menu-cabecalho">Contatos</a>
-                </nav>
-                <a href="login.html" class="menu-cabecalho entrar">Entrar em Contato</a>
+                <div class="carousel-item">
+                    <img src="imagens/casa2.jpg" class="d-block w-100 imagem-carrossel" alt="casa2">
+                </div>
+                <div class="carousel-item">
+                    <img src="imagens/casa3.jpg" class="d-block w-100 imagem-carrossel" alt="casa3">
+                </div>
+                <div class="carousel-item">
+                    <img src="imagens/casa4.jpg" class="d-block w-100 imagem-carrossel" alt="casa4">
+                </div>
             </div>
-        </header>
-
-        <section class="banner">
-            <h2>Seu novo lar te espera</h2>
-            <p>Casas, apartamentos e terrenos à venda</p>
-        </section>
-
-        <section class="imoveis">
-            <h3>Imóveis em Destaque</h3>
-            <div class="card">
-                <img src="https://via.placeholder.com/300x200" alt="Casa 1">
-                <p><strong>Casa 3 Quartos</strong><br>R$ 350.000</p>
-            </div>
-            <div class="card">
-                <img src="https://via.placeholder.com/300x200" alt="Apartamento 2">
-                <p><strong>Apartamento 2 Quartos</strong><br>R$ 250.000</p>
-            </div>
-        </section>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
     </div>
-    <footer>
-        <p>&copy; 2025 Imobiliária Simples. Todos os direitos reservados.</p>
-    </footer>
 
+
+    <h2>Todos as casas</h2>
+    <div class="row">
+        <?php
+        include "conexao.php";
+        $sql = "select * from imobiliaria";
+        $resultado = mysqli_query($conexao, $sql);
+        while ($linha = mysqli_fetch_assoc($resultado)) {
+        ?>
+            <div class="col-3 mb-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="<?= $linha['foto']; ?>" class="card-img-top img-formatada">
+                    <div class="card-body">
+                        <h5 class="card-title"><?= $linha['titulo']; ?></h5>
+                        <p class="card-text"> <?= $linha['descricao']; ?></p>
+                        <a href="umacasa.php?id=<?= $linha['id'] ?>" class="btn btn-primary">Veja detalhes</a>
+                    </div>
+                </div>
+            </div>
+        <?php
+        }
+        ?>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
 </body>
 
 </html>
